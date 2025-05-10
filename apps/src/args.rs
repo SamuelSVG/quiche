6ea5ab34e3e1c2101d28e2c4a4b38fea0f50f57f
Sebastien Vandeneede <sebastien.vandeneede@comexis.net>
2025-05-10 14:50:45 +0200

    fixed none address-discovery given

--- apps/src/args.rs
+++ apps/src/args.rs
@@ -390,7 +390,7 @@ impl Args for ClientArgs {
         let send_priority_update = args.get_bool("--send-priority-update");
 
         let address_discovery =
-            if args.get_bool("--address-discovery") {
+            if args.get_bool("--address-discovery") && args.get_str("--address-discovery") != "<None>" {
                 Some(args.get_str("--address-discovery").parse::<u64>().unwrap())
             } else {
                 None
@@ -508,9 +508,8 @@ impl Args for ServerArgs {
         let disable_pacing = args.get_bool("--disable-pacing");
         let enable_pmtud = args.get_bool("--enable-pmtud");
 
-        // TODO erreur avec unwrap quand on ne met pas --address-discovery dans la commande
         let address_discovery = 
-            if args.get_bool("--address-discovery") {
+            if args.get_bool("--address-discovery") && args.get_str("--address-discovery") != "<None>" {
                 Some(args.get_str("--address-discovery").parse::<u64>().unwrap())
             } else {
                 None
